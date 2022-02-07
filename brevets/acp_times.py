c70805c7c6db83fbcb3232b9da8d4de62f0a4bbe
JoeCates5 <67815262+JoeCates5@users.noreply.github.com>
2022-02-06 16:38:32 -0800

    finished project4

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -5,7 +5,7 @@ following rules described at https://rusa.org/octime_alg.html
 and https://rusa.org/pages/rulesForRiders
 """
 import arrow
-
+import math
 
 #  You MUST provide the following two functions
 #  with these signatures. You must keep
@@ -18,15 +18,66 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
     Args:
        control_dist_km:  number, control distance in kilometers
-       brevet_dist_km: number, nominal distance of the brevet
-           in kilometers, which must be one of 200, 300, 400, 600,
-           or 1000 (the only official ACP brevet distances)
+          brevet_dist_km: number, nominal distance of the brevet
+          in kilometers, which must be one of 200, 300, 400, 600, or 1000
+          (the only official ACP brevet distances)
        brevet_start_time:  An arrow object
     Returns:
-       An arrow object indicating the control open time.
+       An arrow object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    if(control_dist_km == 0):
+        return brevet_start_time
+    totalTime = 0
+    while(control_dist_km > 0):
+        if((control_dist_km <= 1000 and control_dist_km > 600 and brevet_dist_km != 600) or (brevet_dist_km == 1000 and  control_dist_km <= 1200 and control_dist_km > 1000)):
+            print("hello")
+            if(control_dist_km > 1000):
+                control_dist_km = 1000
+            dist = 1000 - control_dist_km if 1000 - control_dist_km != 0 else 400
+            distance = 400 - dist if 400 - dist != 0 else 400
+            totalTime += distance / 28
+            control_dist_km -= distance
+
+        elif((control_dist_km <= 600 and control_dist_km > 400 and brevet_dist_km != 400 ) or (brevet_dist_km == 600 and control_dist_km <= 720 and control_dist_km > 600)):
+            if(control_dist_km > 600):
+                control_dist_km = 600
+            dist = 600 - control_dist_km if 600 - control_dist_km != 0 else 200
+            distance = 200 - dist if 200 - dist != 0 else 200
+            totalTime += distance / 30
+            control_dist_km -= distance
+
+        elif((control_dist_km <= 400 and control_dist_km > 200 and brevet_dist_km != 300 and brevet_dist_km != 200 ) or (brevet_dist_km == 400 and control_dist_km <= 480 and control_dist_km > 400) ):
+            if(control_dist_km > 400):
+                control_dist_km = 400
+            dist = 400 - control_dist_km if 400 - control_dist_km != 0 else 200
+            distance = 200 - dist if 200 - dist != 0 else 200
+            totalTime += distance / 32
+            control_dist_km -= distance
+
+        elif(brevet_dist_km == 300 and control_dist_km <= 360 and control_dist_km > 300):
+            if(control_dist_km > 300):
+                control_dist_km = 300
+            dist = 300 - control_dist_km if 300 - control_dist_km != 0 else 100
+            distance = 200 - dist if 200 - dist != 0 else 100
+            totalTime += distance / 32
+            control_dist_km -= (100 + (control_dist_km - 300))
+
+        elif((control_dist_km <= 200 and control_dist_km > 0) or (brevet_dist_km == 200 and control_dist_km <= 240 and control_dist_km > 200) ):
+            if(control_dist_km > 200):
+                totalTime += 200 / 34
+                control_dist_km -= control_dist_km
+            else:
+                totalTime += control_dist_km / 34
+                control_dist_km = 0
+
+
+    hours = math.floor(totalTime)
+    minutes = round((totalTime - hours) * 60)
+    shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
+    return shiftedTime
+
+
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -41,4 +92,70 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    totalTime = 0
+    if(control_dist_km == 0):
+        shiftedTime = brevet_start_time.shift(hours=1)
+        return shiftedTime
+
+
+    if(control_dist_km >= brevet_dist_km):
+        if(brevet_dist_km == 200):
+            shiftedTime = brevet_start_time.shift(hours=13.5)
+            return shiftedTime
+        if(brevet_dist_km == 400):
+            shiftedTime = brevet_start_time.shift(hours=27)
+            return shiftedTime
+        if(brevet_dist_km == 600):
+            shiftedTime = brevet_start_time.shift(hours=40)
+            return shiftedTime
+        if(brevet_dist_km == 1000):
+            shiftedTime = brevet_start_time.shift(hours=75)
+            return shiftedTime
+
+
+
+    while(control_dist_km > 0):
+        if((control_dist_km <= 1000 and control_dist_km > 600 and brevet_dist_km != 600 ) or (brevet_dist_km == 1000 and  control_dist_km <= 1200 and control_dist_km > 1000)):
+            if(control_dist_km > 1000):
+                control_dist_km = 1000
+
+            dist = 1000 - control_dist_km if 1000 - control_dist_km != 0 else 400
+            distance = 400 - dist if 400 - dist != 0 else 400
+            totalTime += distance / 11.428
+            control_dist_km -= distance
+
+        elif((control_dist_km <= 600 and control_dist_km > 400 and brevet_dist_km != 400 ) or (brevet_dist_km == 600 and  control_dist_km <= 720 and control_dist_km > 600 )):
+            if(control_dist_km > 600):
+                control_dist_km = 600
+            dist = 600 - control_dist_km if 600 - control_dist_km != 0 else 200
+            distance = 200 - dist if 200 - dist != 0 else 200
+            totalTime += distance / 15
+            control_dist_km -= distance
+
+        elif((control_dist_km <= 400 and control_dist_km > 200 and brevet_dist_km != 200 ) or (brevet_dist_km == 400 and  control_dist_km <= 480 and control_dist_km > 400) ):
+            if(control_dist_km > 400):
+                control_dist_km = 400
+
+            dist = 400 - control_dist_km if 400 - control_dist_km != 0 else 200
+            distance = 200 - dist if 200 - dist != 0 else 200
+            totalTime += distance / 15
+            control_dist_km -= distance
+
+        elif(brevet_dist_km == 300 and control_dist_km <= 360 and control_dist_km > 300):
+            totalTime += 300 / 32
+            control_dist_km -= (100 + (control_dist_km - 300))
+
+        elif((control_dist_km <= 200 and control_dist_km > 0) or (brevet_dist_km == 200 and control_dist_km <= 240 and control_dist_km > 200)):
+            if(control_dist_km > 200):
+                totalTime += 200 / 15
+                control_dist_km -= control_dist_km
+            else:
+                totalTime += control_dist_km / 15
+                control_dist_km = 0
+
+    hours = math.floor(totalTime)
+
+    minutes = round((totalTime - hours) * 60)
+
+    shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
+    return shiftedTime
--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -51,14 +51,18 @@ def _calc_times():
     """
     app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
+    distance = request.args.get('distance', 999, type=int)
+    begin_date = request.args.get('begin_date')
     app.logger.debug("km={}".format(km))
+    app.logger.debug("distance={}".format(distance))
+    app.logger.debug("begin_date={}".format(begin_date))
     app.logger.debug("request.args: {}".format(request.args))
     # FIXME!
     # Right now, only the current time is passed as the start time
     # and control distance is fixed to 200
     # You should get these from the webpage!
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
+    open_time = acp_times.open_time(km, distance, arrow.get(begin_date)).format('YYYY-MM-DDTHH:mm')
+    close_time = acp_times.close_time(km, distance, arrow.get(begin_date)).format('YYYY-MM-DDTHH:mm')
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
