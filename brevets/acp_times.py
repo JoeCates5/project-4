9cc79dd451c919716e1a8710ba6f3e58c355b4c7
JoeCates5 <67815262+JoeCates5@users.noreply.github.com>
2022-02-07 14:15:27 -0800

    Update acp_times.py

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -11,7 +11,15 @@ import math
 #  with these signatures. You must keep
 #  these signatures even if you don't use all the
 #  same arguments.
-#
+
+
+def helperFunction(brevit_length, control, dict, previous_bracket):
+    if(control > brevit_length):
+        control = brevit_length
+    distance = control - previous_bracket
+    totalTime = distance / dict.get(previous_bracket)
+    control -= distance
+    return [control, totalTime]
 
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -26,78 +34,37 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    if(control_dist_km == 0):
-        return brevet_start_time
-
+    dict = {0:34, 200:32, 400:30, 600:28}
+    timeSum = 0
     while(control_dist_km > 0):
         if((control_dist_km <= 1000 and control_dist_km > 600 and brevet_dist_km != 600) or (brevet_dist_km == 1000 and  control_dist_km <= 1200 and control_dist_km > 1000)):
-            if(control_dist_km > 1000):
-                control_dist_km = 1000
-            dist = 1000 - control_dist_km if 1000 - control_dist_km != 0 else 400
-            distance = 400 - dist if 400 - dist != 0 else 400
-            totalTime = distance / 28
-            hours = math.floor(totalTime)
-            minutes = round((totalTime - hours) * 60)
-            shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-            brevet_start_time = shiftedTime
-            control_dist_km -= distance
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 600)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
         elif((control_dist_km <= 600 and control_dist_km > 400 and brevet_dist_km != 400 ) or (brevet_dist_km == 600 and control_dist_km <= 720 and control_dist_km > 600)):
-            if(control_dist_km > 600):
-                control_dist_km = 600
-            dist = 600 - control_dist_km if 600 - control_dist_km != 0 else 200
-            distance = 200 - dist if 200 - dist != 0 else 200
-            totalTime = distance / 30
-            hours = math.floor(totalTime)
-            minutes = round((totalTime - hours) * 60)
-            shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-            brevet_start_time = shiftedTime
-            control_dist_km -= distance
-
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 400)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
         elif((control_dist_km <= 400 and control_dist_km > 200 and brevet_dist_km != 200 and brevet_dist_km != 300) or (brevet_dist_km == 400 and control_dist_km <= 480 and control_dist_km > 400) ):
-            if(control_dist_km > 400):
-                control_dist_km = 400
-            dist = 400 - control_dist_km if 400 - control_dist_km != 0 else 200
-            distance = 200 - dist if 200 - dist != 0 else 200
-            totalTime = distance / 32
-            hours = math.floor(totalTime)
-            minutes = round((totalTime - hours) * 60)
-            shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-            brevet_start_time = shiftedTime
-            control_dist_km -= distance
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 200)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
         elif((brevet_dist_km == 300 and control_dist_km <= 360 and control_dist_km > 300) or (brevet_dist_km == 300 and control_dist_km > 200 and  control_dist_km < 300)):
-            if(control_dist_km > 300):
-                control_dist_km = 300
-            dist = 300 - control_dist_km if 300 - control_dist_km != 0 else 100
-            distance = 100 - dist if 200 - dist != 0 else 100
-            totalTime = distance / 32
-            hours = math.floor(totalTime)
-            minutes = round((totalTime - hours) * 60)
-            shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-            brevet_start_time = shiftedTime
-            control_dist_km -= distance
-
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 200)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
         elif((control_dist_km <= 200 and control_dist_km > 0) or (brevet_dist_km == 200 and control_dist_km <= 240 and control_dist_km > 200) ):
-            if(control_dist_km > 200):
-                totalTime = 200 / 34
-                hours = math.floor(totalTime)
-                minutes = round((totalTime - hours) * 60)
-                shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-                brevet_start_time = shiftedTime
-                control_dist_km -= control_dist_km
-            else:
-                totalTime = control_dist_km / 34
-                hours = math.floor(totalTime)
-                minutes = round((totalTime - hours) * 60)
-                shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-                brevet_start_time = shiftedTime
-                control_dist_km = 0
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 0)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
-
-
+    hours = math.floor(timeSum)
+    minutes = round((timeSum - hours) * 60)
+    shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
     return shiftedTime
 
 
@@ -115,7 +82,6 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    totalTime = 0
     if(control_dist_km == 0):
         shiftedTime = brevet_start_time.shift(hours=1)
         return shiftedTime
@@ -136,74 +102,35 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
             return shiftedTime
 
 
-
+    dict = {0:15, 200:15 , 400:15, 600:11.428}
+    timeSum = 0
     while(control_dist_km > 0):
         if((control_dist_km <= 1000 and control_dist_km > 600 and brevet_dist_km != 600) or (brevet_dist_km == 1000 and  control_dist_km <= 1200 and control_dist_km > 1000)):
-            if(control_dist_km > 1000):
-                control_dist_km = 1000
-            dist = 1000 - control_dist_km if 1000 - control_dist_km != 0 else 400
-            distance = 400 - dist if 400 - dist != 0 else 400
-            totalTime = distance / 11.428
-            hours = math.floor(totalTime)
-            minutes = round((totalTime - hours) * 60)
-            shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-            brevet_start_time = shiftedTime
-            control_dist_km -= distance
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 600)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
         elif((control_dist_km <= 600 and control_dist_km > 400 and brevet_dist_km != 400 ) or (brevet_dist_km == 600 and control_dist_km <= 720 and control_dist_km > 600)):
-            if(control_dist_km > 600):
-                control_dist_km = 600
-            dist = 600 - control_dist_km if 600 - control_dist_km != 0 else 200
-            distance = 200 - dist if 200 - dist != 0 else 200
-            totalTime = distance / 15
-            hours = math.floor(totalTime)
-            minutes = round((totalTime - hours) * 60)
-            shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-            brevet_start_time = shiftedTime
-            control_dist_km -= distance
-
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 400)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
         elif((control_dist_km <= 400 and control_dist_km > 200 and brevet_dist_km != 200 and brevet_dist_km != 300) or (brevet_dist_km == 400 and control_dist_km <= 480 and control_dist_km > 400) ):
-            if(control_dist_km > 400):
-                control_dist_km = 400
-            dist = 400 - control_dist_km if 400 - control_dist_km != 0 else 200
-            distance = 200 - dist if 200 - dist != 0 else 200
-            totalTime = distance / 15
-            hours = math.floor(totalTime)
-            minutes = round((totalTime - hours) * 60)
-            shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-            brevet_start_time = shiftedTime
-            control_dist_km -= distance
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 200)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
         elif((brevet_dist_km == 300 and control_dist_km <= 360 and control_dist_km > 300) or (brevet_dist_km == 300 and control_dist_km > 200 and  control_dist_km < 300)):
-            if(control_dist_km > 300):
-                flag = true
-                control_dist_km = 300
-            dist = 300 - control_dist_km if 300 - control_dist_km != 0 else 100
-            distance = 100 - dist if 200 - dist != 0 else 100
-            totalTime = distance / 15
-            hours = math.floor(totalTime)
-            minutes = round((totalTime - hours) * 60)
-            shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-            brevet_start_time = shiftedTime
-            control_dist_km -= distance
-
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 200)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
         elif((control_dist_km <= 200 and control_dist_km > 0) or (brevet_dist_km == 200 and control_dist_km <= 240 and control_dist_km > 200) ):
-            if(control_dist_km > 200):
-                totalTime = 200 / 34
-                hours = math.floor(totalTime)
-                minutes = round((totalTime - hours) * 60)
-                shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-                brevet_start_time = shiftedTime
-                control_dist_km -= control_dist_km
-            else:
-                totalTime = control_dist_km / 15
-                hours = math.floor(totalTime)
-                minutes = round((totalTime - hours) * 60)
-                shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
-                brevet_start_time = shiftedTime
-                control_dist_km = 0
+            returnList = helperFunction(brevet_dist_km, control_dist_km, dict, 0)
+            control_dist_km = returnList[0]
+            timeSum += returnList[1]
 
-
+    hours = math.floor(timeSum)
+    minutes = round((timeSum - hours) * 60)
+    shiftedTime = brevet_start_time.shift(hours=hours, minutes=minutes)
     return shiftedTime
